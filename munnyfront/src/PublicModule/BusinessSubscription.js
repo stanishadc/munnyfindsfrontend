ed7e877389d197523e08627e096f66af89440d1a
Tanisha <stanisha.dc@gmail.com>
2021-09-21 16:26:45 +0530

    payment

--- munnyfront/src/PublicModule/BusinessSubscription.js
+++ munnyfront/src/PublicModule/BusinessSubscription.js
@@ -38,47 +38,43 @@ export default function BusinessSubscription(props) {
     SubsciptionPayment();
   };
   async function SubsciptionPayment() {
-    applicationAPI()
-    .fetchpayment()
-    .then((res) => console.log(res))
-    .catch(function (error) {
-      if (error.response) {
-        console.log(error.response);
-      }
-    });
-  }  
-  const applicationAPI = () => {
-    const headerconfig = {
-      headers: {
-        Authorization: `Bearer FLWSECK_TEST-b2007d4a05c806c7d4284700d9366c84-X`,
-      },
-    };
-     const body = {
+    var data = JSON.stringify({
       tx_ref: "hooli-tx-1920bbtytty",
       amount: "100",
       currency: "NGN",
       redirect_url: "https://webhook.site/9d0b00ba-9a69-44fa-a43d-a82c33c36fdc",
       payment_options: "card",
-      meta: {
-        consumer_id: 23,
-        consumer_mac: "92a3-912ba-1192a",
-      },
+      meta: { consumer_id: 23, consumer_mac: "92a3-912ba-1192a" },
       customer: {
         email: "user@gmail.com",
         phonenumber: "080****4528",
-        name: "Yemi Desola"
+        name: "Yemi Desola",
       },
       customizations: {
         title: "Pied Piper Payments",
         description: "Middleout isn't free. Pay the price",
-        logo: "https://assets.piedpiper.com/logo.png"
-      }
+        logo: "https://assets.piedpiper.com/logo.png",
+      },
+    });
+
+    var config = {
+      method: "post",
+      url: "https://api.flutterwave.com/v3/payments",
+      headers: {
+        Authorization: "Bearer FLWSECK_TEST-b2007d4a05c806c7d4284700d9366c84-X",
+        "Content-Type": "application/json",
+      },
+      data: data,
     };
-    return {
-      fetchpayment: () =>
-        axios.post("https://api.flutterwave.com/v3/payments", body, headerconfig)
-    };
-  };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
   return (
     <div>
       <div id="main-wrapper">
