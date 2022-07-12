f7af563a7807f97033a8f2568e033e4801b4e936
tanisha <stanisha.dc@gmail.com>
2022-07-12 10:48:29 +0530

    logout

--- munnyfront/src/CommonFiles/BusinessProtected.js
+++ munnyfront/src/CommonFiles/BusinessProtected.js
@@ -6,8 +6,10 @@ export const BusinessProtected = ({ component: Component, ...rest }) => {
     return (
         <Route{...rest}
             render={(props) => {
-                if (localStorage.getItem('MFFBusinessUserId').length>0) {
-                    return <Component{...props} />
+                if (localStorage.getItem('MFFBusinessUserId') !== null) {
+                    if (localStorage.getItem('MFFBusinessUserId').length > 0) {
+                        return <Component{...props} />
+                    }
                 }
                 else {
                     return (
